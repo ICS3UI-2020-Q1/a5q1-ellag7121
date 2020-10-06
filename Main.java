37be00c4f89a8a29a31d476b025254d4335c2c3c
ellag7121 <ellag7121@wrdsb.ca>
2020-10-06 13:02:54 +0000

    finished a5q1!

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * ask the user for a number, tell them the multiplication table for 1 to 12 with that number
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //initialize variables
+    int number; //the number provided by the user
+    int product; //the product of the the given number and the current value of "i" in the for loop
+
+    //ask user for number
+    System.out.println("Please enter an integer to create a multiplication table for");
+    number = input.nextInt(); //store it in the variable
+
+    //make a for loop to count 12 times to make the multiplication table
+    for(int i = 1; i <= 12; i++){
+      //find the product of both numbers
+      product = number * i;
+      //print the current line in the multiplication table
+      System.out.println(i + " x " + number + " = " + product);
+    }
     
   }
 }
